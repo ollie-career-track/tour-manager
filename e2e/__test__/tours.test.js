bd7f100aaf2443178e0c6d264513f45664873699
olliecomet <leyechi@gmail.com>
2019-10-02 02:20:48 -0700

    passing get all tours tests

--- e2e/__test__/tours.test.js
+++ e2e/__test__/tours.test.js
@@ -14,7 +14,7 @@ describe('tours api', () => {
   };
 
   function postTour(tour) {
-    return request 
+    return request
       .post('/api/tours')
       .send(tour)
       .expect(200)
@@ -43,6 +43,27 @@ describe('tours api', () => {
   });
 
   it('gets all tours', () => {
+    const firstTour = {
+      title: 'first tour',
+      activities: ['touring', 'more touring'],
+      stops: []
+    };
 
+    return Promise.all([
+      postTour(firstTour),
+      postTour({ title: 'second tour', activities: ['touring', 'more touring'], stops: [] }),
+      postTour({ title: 'third tour', activities: ['touring', 'more touring'], stops: [] })
+    ])
+      .then(() => {
+        return request.get('/api/tours').expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.length).toBe(3);
+        expect(body[0]).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          ...tour
+        });
+      });
   });
 });
